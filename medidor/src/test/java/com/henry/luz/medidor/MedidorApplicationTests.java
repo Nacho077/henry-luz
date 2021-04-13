82303a22f902d6723ea8c4ab7d0b70d1f54cf5dd
Ignacio Gimenez <ignaciogimenez70@gmail.com>
2021-04-13 00:16:06 -0300

    first commit

---
+++ medidor/src/test/java/com/henry/luz/medidor/MedidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henry.luz.medidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
