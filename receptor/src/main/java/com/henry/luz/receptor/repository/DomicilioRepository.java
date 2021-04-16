0fb2c309fc5e986c44b617ca7d70bb3db008aeb4
Ignacio Gimenez <ignaciogimenez70@gmail.com>
2021-04-16 11:32:38 -0300

    refactor

--- receptor/src/main/java/com/henry/luz/receptor/repository/DomicilioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.henry.luz.receptor.repository;
-
-import com.henry.luz.receptor.model.Domicilio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DomicilioRepository extends JpaRepository<Domicilio, Integer> {
-}
